a10aa75bcfa42030b141050df413a308262163ce
Ken VanDine <ken@vandine.org>
2023-03-19 10:53:19 -0400

    Basic plugin structure

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java")
+    id("xyz.jpenilla.run-paper") version "2.0.1" // Adds runServer and runMojangMappedServer tasks for testing
+}
+
+group = "org.vandine"
+version = "0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(18))
+}
+
+
+tasks {
+    runServer {
+        // Configure the Minecraft version for our task.
+        // This is the only required configuration besides applying the plugin.
+        // Your plugin's jar (or shadowJar if present) will be used automatically.
+        minecraftVersion("1.19.3")
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "papermc-whackyplugin"
+
