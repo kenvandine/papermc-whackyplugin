ff829dc8036a61f9689a896555ed6e09f263a116
Ken VanDine <ken@vandine.org>
2023-03-19 19:46:04 -0400

    Added block handler

--- src/main/java/org/vandine/whackyplugin/WhackyPlugin.java
+++ src/main/java/org/vandine/whackyplugin/WhackyPlugin.java
@@ -2,9 +2,12 @@ package org.vandine.whackyplugin;
 
         import net.kyori.adventure.text.Component;
         import org.bukkit.Bukkit;
+        import org.bukkit.Material;
+        import org.bukkit.block.Block;
         import org.bukkit.event.EventHandler;
         import org.bukkit.event.Listener;
         import org.bukkit.event.player.PlayerJoinEvent;
+        import org.bukkit.event.block.BlockPlaceEvent;
         import org.bukkit.plugin.java.JavaPlugin;
 
 public class WhackyPlugin extends JavaPlugin implements Listener {
@@ -17,6 +20,11 @@ public class WhackyPlugin extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + ", welcome to Ken's development server!"));
-        event.getPlayer().sendMessage(Component.text("Drew says hi"));
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Block b = event.getBlock();
+        b.setType(Material.DIAMOND_BLOCK);
     }
 }
